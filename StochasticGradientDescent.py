b2ad31c6ab17a234744f9bd7b257633ccd53c4ea
Isaac Buitrago <mightyking7@gmail.com>
2020-10-10 17:53:26 -0500

    Updated sgd

--- StochasticGradientDescent.py
+++ StochasticGradientDescent.py
@@ -1,5 +1,12 @@
 import numpy as np
+import seaborn as sns
+from GradientDescent import plotLL
+from sklearn.preprocessing import StandardScaler
 from GradientDescent import linearPredict, softmax, cross_entropy
+from data import df_train_digits, df_test_digits, df_train_news, df_test_news
+
+sns.set_theme(style="darkgrid")
+
 
 def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         biases: np.ndarray, lr: float, n_cls: int,
@@ -9,7 +16,7 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
     """
     n = len(y)
 
-    L1 = 2 / n * W
+    L2 = np.sum(1 / n * W)
 
     axis0 = np.arange(X.shape[0])
 
@@ -24,13 +31,13 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         for i in range(n):
             index = np.random.randint(0, n)
 
-            logitScores = linearPredict(X[index, :], W, biases, n_cls)
+            logitScores = linearPredict(X[index, :], W, biases)
             probs = softmax(logitScores, n_cls)
 
-            cost += cross_entropy(probs, y[index])
+            cost += cross_entropy(probs, y[index], W)
 
             # gradient of weights
-            gradsW = (probs[axis0, y.squeeze(-1)] - 1).T.dot(X)
+            gradsW = (probs[axis0, y.squeeze(-1)] - 1).T.dot(X) + L2
             gradsBiases = np.sum(probs, axis=0).reshape(-1, 1)
 
             # update weights
@@ -42,3 +49,54 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         cost_history[it] = cost
 
     return W, biases, cost_history
+
+def main():
+    """
+    N output classes in datasets
+    """
+    n_cls_digits = len(np.unique(df_train_digits['Var2'].values))
+    n_cls_news = len(np.unique(df_train_news['Var2'].values))
+
+    """
+    N features to learn for each dataset
+    """
+    n_feats_digits = df_train_digits.shape[1] - 1
+    n_feats_news = df_train_news.shape[1] - 1
+
+    lr = .01
+    iterations = 100
+
+    """
+    Weight matrices and biases for each dataset
+    """
+    W_digits = np.random.rand(n_cls_digits, n_feats_digits)
+    biases_digits = np.random.rand(n_cls_digits, 1)
+
+    # W_news = np.random.rand(n_cls_news, n_feats_news)
+    # biases_news = np.random.rand(n_cls_news, 1)
+
+    """
+    Feature and label vectors
+    """
+    X_digits = df_train_digits.loc[:, :'X_train_65'].to_numpy()
+    y_digits = df_train_digits.loc[:, 'Var2'].to_numpy().reshape(-1, 1)
+
+    # X_news = df_train_news.loc[:, :'X_train_2001'].to_numpy()
+    # y_news = df_train_news.loc[:, 'Var2'].to_numpy().reshape(-1, 1)
+
+    """
+    Normalize datasets
+    """
+    X_digits = X_digits / 255.
+
+    scaler = StandardScaler()
+    # X_news = scaler.fit_transform(X_news)
+
+    # train
+    W_digits, biases_digits, LL_digits = sgd(X_digits, y_digits, W_digits, biases_digits, lr, n_cls_digits, iterations)
+    print(LL_digits)
+    # W_news, biases_news, LL_news = sgd(X_news, y_news, W_news, biases_news, lr, n_cls_news, iterations)
+
+    # plot log likelihood
+    plotLL(LL_digits, iterations, "digits", "SGD")
+    # plotLL(LL_news, iterations, "news", "GD")
