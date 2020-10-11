66675a19fad74553347bfe6fac3aec2d1cc3d7aa
Isaac Buitrago <mightyking7@gmail.com>
2020-10-11 13:18:15 -0500

    Fixed index error

--- StochasticGradientDescent.py
+++ StochasticGradientDescent.py
@@ -18,8 +18,6 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
 
     L2 = np.sum(1 / n * W)
 
-    axis0 = np.arange(X.shape[0])
-
     cost_history = np.zeros(iterations)
 
     for it in range(iterations):
@@ -27,17 +25,21 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         cost = 0.0
 
         # update weight vector based on one datapoint at a time.
-
         for i in range(n):
             index = np.random.randint(0, n)
 
-            logitScores = linearPredict(X[index, :], W, biases)
+            data = X[index, :].reshape(1, -1)
+            label = y[index]
+
+            logitScores = linearPredict(data, W, biases)
             probs = softmax(logitScores, n_cls)
 
-            cost += cross_entropy(probs, y[index], W)
+            cost += cross_entropy(probs, label, W)
+
+            z = probs[0, label] - 1
 
             # gradient of weights
-            gradsW = (probs[axis0, y.squeeze(-1)] - 1).T.dot(X) + L2
+            gradsW = z.dot(data) + L2
             gradsBiases = np.sum(probs, axis=0).reshape(-1, 1)
 
             # update weights
@@ -50,53 +52,51 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
 
     return W, biases, cost_history
 
-def main():
-    """
-    N output classes in datasets
-    """
-    n_cls_digits = len(np.unique(df_train_digits['Var2'].values))
-    n_cls_news = len(np.unique(df_train_news['Var2'].values))
+"""
+N output classes in datasets
+"""
+n_cls_digits = len(np.unique(df_train_digits['Var2'].values))
+n_cls_news = len(np.unique(df_train_news['Var2'].values))
 
-    """
-    N features to learn for each dataset
-    """
-    n_feats_digits = df_train_digits.shape[1] - 1
-    n_feats_news = df_train_news.shape[1] - 1
+"""
+N features to learn for each dataset
+"""
+n_feats_digits = df_train_digits.shape[1] - 1
+n_feats_news = df_train_news.shape[1] - 1
 
-    lr = .01
-    iterations = 100
+lr = .01
+iterations = 100
 
-    """
-    Weight matrices and biases for each dataset
-    """
-    W_digits = np.random.rand(n_cls_digits, n_feats_digits)
-    biases_digits = np.random.rand(n_cls_digits, 1)
+"""
+Weight matrices and biases for each dataset
+"""
+W_digits = np.random.rand(n_cls_digits, n_feats_digits)
+biases_digits = np.random.rand(n_cls_digits, 1)
 
-    # W_news = np.random.rand(n_cls_news, n_feats_news)
-    # biases_news = np.random.rand(n_cls_news, 1)
+# W_news = np.random.rand(n_cls_news, n_feats_news)
+# biases_news = np.random.rand(n_cls_news, 1)
 
-    """
-    Feature and label vectors
-    """
-    X_digits = df_train_digits.loc[:, :'X_train_65'].to_numpy()
-    y_digits = df_train_digits.loc[:, 'Var2'].to_numpy().reshape(-1, 1)
+"""
+Feature and label vectors
+"""
+X_digits = df_train_digits.loc[:, :'X_train_65'].to_numpy()
+y_digits = df_train_digits.loc[:, 'Var2'].to_numpy().reshape(-1, 1)
 
-    # X_news = df_train_news.loc[:, :'X_train_2001'].to_numpy()
-    # y_news = df_train_news.loc[:, 'Var2'].to_numpy().reshape(-1, 1)
+# X_news = df_train_news.loc[:, :'X_train_2001'].to_numpy()
+# y_news = df_train_news.loc[:, 'Var2'].to_numpy().reshape(-1, 1)
 
-    """
-    Normalize datasets
-    """
-    X_digits = X_digits / 255.
+"""
+Normalize datasets
+"""
+X_digits = X_digits / 255.
 
-    scaler = StandardScaler()
-    # X_news = scaler.fit_transform(X_news)
+scaler = StandardScaler()
+# X_news = scaler.fit_transform(X_news)
 
-    # train
-    W_digits, biases_digits, LL_digits = sgd(X_digits, y_digits, W_digits, biases_digits, lr, n_cls_digits, iterations)
-    print(LL_digits)
-    # W_news, biases_news, LL_news = sgd(X_news, y_news, W_news, biases_news, lr, n_cls_news, iterations)
+# train
+W_digits, biases_digits, LL_digits = sgd(X_digits, y_digits, W_digits, biases_digits, lr, n_cls_digits, iterations)
+# W_news, biases_news, LL_news = sgd(X_news, y_news, W_news, biases_news, lr, n_cls_news, iterations)
 
-    # plot log likelihood
-    plotLL(LL_digits, iterations, "digits", "SGD")
-    # plotLL(LL_news, iterations, "news", "GD")
+# plot log likelihood
+plotLL(LL_digits, iterations, "digits", "SGD")
+# plotLL(LL_news, iterations, "news", "GD")
