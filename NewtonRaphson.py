d960c43cc51ac1e3257d13da2e47b686361cdefa
Isaac Buitrago <mightyking7@gmail.com>
2020-10-12 16:44:50 -0500

    Fixed newton rapshon

--- NewtonRaphson.py
+++ NewtonRaphson.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List
+from logits import linearPredict, softmax, cross_entropy
 
 def nr(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         biases: np.ndarray, lr: float, n_cls: int,
@@ -8,8 +9,12 @@ def nr(X: np.ndarray, y: np.ndarray, W: np.ndarray,
     Newton raphson optimization
     """
     n = len(y)
+    lam = 1/n
+    n_feats = W.shape[1]
 
-    L2 = np.sum(1 / n * W)
+    L2 = lam * (W.T.dot(W))
+
+    reg = lam * np.eye(n_feats)
 
     axis0 = np.arange(X.shape[0])
 
@@ -22,21 +27,22 @@ def nr(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         # error
         cost_history[it] = cross_entropy(probs, y, W)
 
-        pi = np.prod(probs, axis=1)
+        pi = np.sum(probs * (1 - probs), axis=1)
 
-        diagonal = np.expand_dims(pi, axis=0) * np.identity(len(pi))
+        D = np.expand_dims(pi, axis=0) * np.identity(len(pi))
 
         # compute error for probability of correct outcome
         probs[axis0, y.squeeze(-1)] -= 1
 
         # n_feats x n_feats
-        hessian = X.T.dot(diagonal).dot(X)
+        hessian = X.T.dot(D).dot(X)
+        hessian = hessian + reg
 
         # first derivative w.r.t weights
-        firstD = probs.T.dot(X)
+        firstD = probs.T.dot(X).T
 
         # gradient of weights and biases
-        gradsW = np.linalg.inv(hessian) * firstD
+        gradsW = np.linalg.inv(hessian).dot(firstD).T
 
         gradsBiases = np.sum(probs, axis=0).reshape(-1, 1)
 
@@ -46,3 +52,4 @@ def nr(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         # update biases
         biases -= (lr * gradsBiases)
 
+    return W, biases, cost_history
