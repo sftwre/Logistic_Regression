97c1b5910d6740e4c0eb8e41901e2812ca3054cf
Isaac Buitrago <mightyking7@gmail.com>
2020-10-12 17:00:11 -0500

    Moved training setup into train.py

--- StochasticGradientDescent.py
+++ StochasticGradientDescent.py
@@ -1,7 +1,5 @@
 import numpy as np
-from logits import linearPredict, softmax, cross_entropy, predict, plotLL
-from data import df_train_digits, df_test_digits, df_train_news, df_test_news
-
+from logits import linearPredict, softmax, cross_entropy
 
 def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
         biases: np.ndarray, lr: float, n_cls: int,
@@ -52,34 +50,3 @@ def sgd(X: np.ndarray, y: np.ndarray, W: np.ndarray,
             lr /= 10
 
     return W, biases, cost_history
-
-"""
-N output classes in datasets
-"""
-n_cls_digits = len(np.unique(df_train_digits['Var2'].values))
-n_cls_news = len(np.unique(df_train_news['Var2'].values))
-
-"""
-N features to learn for each dataset
-"""
-n_feats_digits = df_train_digits.shape[1] - 1
-n_feats_news = df_train_news.shape[1] - 1
-
-lr = .01
-iterations = 100
-
-"""
-Weight matrices and biases for each dataset
-"""
-W_digits = np.random.rand(n_cls_digits, n_feats_digits)
-biases_digits = np.random.rand(n_cls_digits, 1)
-
-# W_news = np.random.rand(n_cls_news, n_feats_news)
-# biases_news = np.random.rand(n_cls_news, 1)
-
-
-# train
-W_digits, biases_digits, LL_digits = sgd(X_digits, y_digits, W_digits, biases_digits, lr, n_cls_digits, iterations)
-# W_news, biases_news, LL_news = sgd(X_news, y_news, W_news, biases_news, lr, n_cls_news, iterations)
-
-# plot log likelihood
