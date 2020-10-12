712d8764afd0f65704abc5a3751db11dd5fa1600
Isaac Buitrago <mightyking7@gmail.com>
2020-10-12 06:39:03 -0500

    Refactored to operate on dataset independent weights and biases

--- train.py
+++ train.py
@@ -25,44 +25,38 @@ def main(args):
         print(f"ERROR: -optim must be within {optimizations.keys()}")
         exit(-1)
 
-    # load news dataset and run gd
+    # train on news dataset
     if dataset == datasets[0]:
-        n_cls_news = len(np.unique(y_news))
-        n_feats_news = X_news.shape[1]
+        n_cls = len(np.unique(y_news))
+        n_feats = X_news.shape[1]
+        X = X_news
+        X_test = X_news_test
+        y = y_news
+        y_test = y_news_test
 
-        W_news = np.random.rand(n_cls_news, n_feats_news)
-        biases_news = np.random.rand(n_cls_news, 1)
-
-        optimizer = optimizations[optim]
-
-        W_news, biases_news, LL_news = optimizer(X_news, y_news, W_news, biases_news, lr, n_cls_news, iterations)
-
-        # predict test accuracy
-        acc = predict(X_news_test, y_news_test, W_news, biases_news, n_cls_news)
-        print(f"News accuracy: {acc:.2f}%")
-
-        # plot log likelihood
-        plotLL(LL_news, iterations, dataset, optim, lr, acc)
-
-
+    # train on digits dataset
     else:
-        n_cls_digits = len(np.unique(y_digits))
-        n_feats_digits = X_digits.shape[1]
+        n_cls = len(np.unique(y_digits))
+        n_feats = X_digits.shape[1]
+        X = X_digits
+        X_test = X_digits_test
+        y = y_digits
+        y_test = y_digits_test
 
-        W_digits = np.random.rand(n_cls_digits, n_feats_digits)
-        biases_digits = np.random.rand(n_cls_digits, 1)
+    # init weights and biases
+    W = np.random.rand(n_cls, n_feats)
+    biases = np.random.rand(n_cls, 1)
 
-        optimizer = optimizations[optim]
+    optimizer = optimizations[optim]
 
-        # train
-        W_digits, biases_digits, LL_digits = optimizer(X_digits, y_digits, W_digits, biases_digits, lr, n_cls_digits, iterations)
+    W, biases, LL = optimizer(X, y, W, biases, lr, n_cls, iterations)
 
-        # predict test accuracy
-        acc = predict(X_digits_test, y_digits_test, W_digits, biases_digits, n_cls_digits)
-        print(f"Digits accuracy: {acc:.2f}%")
+    # predict test accuracy
+    acc = predict(X_test, y_test, W, biases, n_cls)
+    print(f"{dataset} accuracy: {acc:.2f}%")
 
-        # plot log likelihood
-        plotLL(LL_digits, iterations, dataset, optim, lr, acc)
+    # plot log likelihood
+    plotLL(LL, iterations, dataset, optim, lr, acc)
 
 
 if  __name__ == "__main__":
